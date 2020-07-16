26e35af57fc0631f8bef850397dcb8037717062c
rwparrish <rino3717@gmail.com>
2020-07-16 11:46:59 -0600

    remove list_id from item serializer

--- shopit-backend/app/serializers/item_serializer.rb
+++ shopit-backend/app/serializers/item_serializer.rb
@@ -1,6 +1,6 @@
 class ItemSerializer < ActiveModel::Serializer
 
-  attributes :id, :name, :description, :bought, :quantity, :list_id
+  attributes :id, :name, :description, :bought, :quantity
   belongs_to :list
   
 end
