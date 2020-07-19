aa220d155fbb62c4f137658830fb11992cbe2d7f
rwparrish <rino3717@gmail.com>
2020-07-19 17:40:01 -0600

    refactor show page to add add-item button

--- shopit-frontend/src/item.js
+++ shopit-frontend/src/item.js
@@ -11,10 +11,129 @@ class Item {
     renderItem() {
         return `
             <li id="item-${this.id}"> 
-            ${this.name} ${this.description} ${this.quantity} ${this.bought ? "bought" : "Not yet bought"}
+            ${this.name} - ${this.description} - ${this.quantity} - ${this.bought ? "bought" : "Not yet bought"}
             <button id='delete' data-id='${this.id}'>Delete</button>
             <button id='update-item' data-id='${this.id}'>Edit</button>  
             </li> `
     }
 
 }
+
+function displayCreateItemForm() {
+    let itemFromDiv = document.getElementById('item-form')
+    let listId = Number(event.target.dataset.id)
+    let html = `
+        <form>
+            <label>Name</label>
+            <input type="text" id="name">
+            <label>Description</label>
+            <input type="text" id="description">
+            <label>Bought</label>
+            <input type="checkbox" id="bought">
+            <label>Quantity</label>
+            <input type="text" id="quantity">
+            <input type="hidden" id="list_id" value="${listId}">
+            <input type="submit">
+        </form>
+    `
+    itemFromDiv.innerHTML = html
+    document.querySelector('form').addEventListener('submit', createItem)
+}
+
+function createItem() {
+    event.preventDefault()
+    const item = {
+        name: document.getElementById('name').value,
+        description: document.getElementById('description').value,
+        baught: document.getElementById('bought').checked,
+        quantity: document.getElementById('quantity').value,
+        list_id: document.getElementById('list_id').value
+    }
+
+    fetch(BASE_URL+'/items', {
+        method: 'POST',
+        body: JSON.stringify(item),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json' 
+        }
+    })
+    .then(resp => resp.json())
+    .then(item => {
+            const showItems = document.querySelector('#show-list ul')
+            let newItem = new Item(item)
+            showItems.innerHTML += newItem.renderItem()
+            attachClickToLinks()
+            clearForm()
+        })
+}
+function deleteItem() {
+    event.preventDefault()
+    clearForm()
+
+    fetch(BASE_URL+`/items/${event.target.dataset.id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json' 
+        }
+    })
+    .then(event.target.parentElement.remove())
+}
+
+function editItem() {
+    event.preventDefault()
+    clearForm()
+    let id = event.target.dataset.id
+    // console.log(id)
+    fetch(BASE_URL+`/items/${id}`)
+    .then(resp => resp.json())
+    .then(item => {
+        let itemFromDiv = document.getElementById('item-form')
+        console.log(item)
+        let html = `
+            <form data-id="${id}">
+                <label>Name</label>
+                <input type="text" id="name" value="${item.name}">
+                <label>Description</label>
+                <input type="text" id="description" value="${item.description}">
+                <label>Bought</label>
+                <input type="checkbox" id="bought" ${item.bought ? "checked" : ""}>
+                <label>Quantity</label>
+                <input type="text" id="quantity" value="${item.quantity}">
+                <input type="hidden" id="list_id" value="${item.list.id}">
+                <input type="submit">
+            </form>
+            `
+    itemFromDiv.innerHTML = html
+    document.querySelector('form').addEventListener('submit', updateItem)
+    })
+}
+
+function updateItem() {
+    event.preventDefault()
+    let upItem = {
+        name: document.getElementById('name').value,
+        description: document.getElementById('description').value,
+        baught: document.getElementById('bought').checked,
+        quantity: document.getElementById('quantity').value,
+        list_id: document.getElementById('list_id').value
+    }
+
+    fetch(BASE_URL+`/items/${event.target.dataset.id}`, {
+        method: 'PATCH',
+        body: JSON.stringify(upItem),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json' 
+        }
+    })
+    .then(resp => resp.json())
+    .then(item => {
+            let newItem = new Item(item)
+            document.querySelector(`li#list-${item.list.id} #items li#item-${item.id}`).innerHTML = newItem.renderItem()
+            attachClickToLinks()
+            clearForm()
+        })
+}
+   
--- shopit-frontend/src/list.js
+++ shopit-frontend/src/list.js
@@ -9,20 +9,81 @@ class List {
         return `
             <li id="list-${this.id}">
                 <a href="#" data-id="${this.id}">${this.name}</a>
-                <button id='add-item' data-id='${this.id}'>Add Item</button>
                 <ul id="items">
                 </ul>
                         
             </li> `
     }
+    // <ul id="items-${this.id}"></ul> - it will affect line 21 and I want to track list id's for items
 
     renderUls() {
         let ul = document.querySelector(`li#list-${this.id} #items`)
             this.items.forEach(item => {
-            ul.innerHTML += `<li>${item.name}
+            ul.innerHTML += `<li>${item.name} - ${item.description} - ${item.quantity} - ${item.bought ? "bought" : "Not yet bought"}
             <button id='delete' data-id='${item.id}'>Delete</button>
             <button id='update-item' data-id='${item.id}'>Edit</button>
             </li>`
         })
     }
 }
+
+function displayCreateForm() {
+    let listFromDiv = document.getElementById('list-form')
+    let html = `
+        <form>
+            <label>Name</label>
+            <input type="text" id="name">
+            <input type="submit">
+        </form>
+    `
+    listFromDiv.innerHTML = html
+    document.querySelector('form').addEventListener('submit', createList)
+}
+
+function createList() {
+    event.preventDefault()
+    const list = {
+        name: document.getElementById('name').value
+    }
+
+    fetch(BASE_URL+'/lists', {
+        method: 'POST',
+        body: JSON.stringify(list),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json' 
+        }
+    })
+    .then(resp => resp.json())
+    .then(list => {
+            const showLists = document.querySelector('#show-lists ul')
+            let shoppingList = new List(list)
+            showLists.innerHTML += shoppingList.renderList()
+            shoppingList.renderUls()
+            attachClickToLinks()
+            clearForm()
+        })
+}
+
+function displayList() {
+    
+    clearForm()
+    clearUls()
+    let id = event.target.dataset.id 
+    let showList = document.getElementById('show-list')
+    fetch(BASE_URL+'/lists/'+id)
+    .then(resp => resp.json())
+    .then(list => {
+        
+        showList.innerHTML += `<h3>${list.name}</h3>
+        <button id='add-item' data-id='${list.id}'>Add Item</button> <ul></ul>`
+            list.items.forEach(item => {
+            document.querySelector('#show-list ul').innerHTML += `<li>${item.name} - ${item.description} - ${item.quantity} - ${item.bought ? "bought" : "Not yet bought"}
+            <button id='delete' data-id='${item.id}'>Delete</button>
+            <button id='update-item' data-id='${item.id}'>Edit</button>
+            </li>`
+        })
+        attachClickToLinks()
+    })
+}
+
